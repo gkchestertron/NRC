0bff8d0dcfbed2c560b271ba3be19f2acd314781
gkchesterton <john.fellman@gmail.com>
2014-01-29 10:37:32 -0800

    fixing some db stuff

--- db/migrate/20140126193017_create_users.rb
+++ db/migrate/20140126193017_create_users.rb
@@ -5,7 +5,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :session_token
       t.string :name, null: false
       t.string :email, null: false
-      t.string :admin, null: false, default: false
+      t.boolean :admin
 
       t.timestamps
     end
--- db/migrate/20140127062610_change_admin_on_users.rb
+++ db/migrate/20140127062610_change_admin_on_users.rb
@@ -1,5 +1,5 @@
 class ChangeAdminOnUsers < ActiveRecord::Migration
   def change
-  	change_column :users, :admin, :boolean
+  	
   end
 end
