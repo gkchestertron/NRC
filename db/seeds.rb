5234d02c039863e0eec0ceae92a4a60e81bdc239
gkchesterton <john.fellman@gmail.com>
2014-01-29 11:36:56 -0800

    adds some seeds and css

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,53 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Page.create(title: 'Home')
+Page.create(title: 'About')
+Page.create(title: 'Contact')
+
+Layout.create(name: 'home', page_id: 1, html: '<div class="row">
+  <div class="col-xs-12">
+    <h1 id="title" class="editable">Title</h1>
+  </div>
+</div>
+<div class="row">
+  <div class="col-xs-12">
+    <p id="description" class="editable">description</p>
+    <p id="description2" class="editable">description2</p>
+
+  </div>
+</div>
+
+')
+Layout.create(name: 'home', page_id: 2, html: '<div class="row">
+  <div class="col-xs-12">
+    <h1 id="title" class="editable">Title</h1>
+  </div>
+</div>
+<div class="row">
+  <div class="col-xs-12">
+    <p id="description" class="editable">description</p>
+    <p id="description2" class="editable">description2</p>
+
+  </div>
+</div>
+
+')
+Layout.create(name: 'home', page_id: 3, html: '<div class="row">
+  <div class="col-xs-12">
+    <h1 id="title" class="editable">Title</h1>
+  </div>
+</div>
+<div class="row">
+  <div class="col-xs-12">
+    <p id="description" class="editable">description</p>
+    <p id="description2" class="editable">description2</p>
+
+  </div>
+</div>
+
+')
+
+u = User.create(email: 'guest@nrccms.com', password: 'foobies')
+u.admin = true
+u.save!
